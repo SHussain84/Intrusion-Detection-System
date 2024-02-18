6ce5aa03248ea386fd0499aaf04920cb03f5e61d
SHussain84 <s.hussain84@edu.salford.ac.uk>
2024-02-18 13:18:21 -0500

    Added functionality to detect SSH attempts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from scapy.all import *
+from scapy.all import sniff, ICMP, IP, TCP
 
 conf.iface = "eth0"
 def packet_callback(packet):
@@ -7,6 +7,10 @@ def packet_callback(packet):
         src_ip = packet[IP].src
         dst_ip = packet[IP].dst
         print(f"Ping detected: {src_ip} -> {dst_ip}")
+    elif TCP in packet and packet[TCP].dport == 22:
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dst
+        print(f"SSH connection attempt detected from {src_ip} to {dst_ip}")
 
 # Sniff traffic on the network interface
-sniff(prn=packet_callback, filter="icmp", store=0)
+sniff(prn=packet_callback, filter="icmp or (tcp and port 22)", store=0)
