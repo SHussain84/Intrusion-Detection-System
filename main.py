90b630098b0236a9650cd6d68ef1f782cc80e06e
SHussain84 <s.hussain84@edu.salford.ac.uk>
2024-02-18 18:15:22 -0500

    Added file reading capability from detection_rules.txt

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-from scapy.all import sniff, ICMP, IP, TCP
+from scapy.all import *
 
 conf.iface = "eth0"
+rules = []
+
+def read_detection_rules(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip().lower()
+            if line in ['ping', 'icmp']:
+                rules.append("icmp")
+            elif line == 'ssh':
+                rules.append("(tcp and port 22)")
+            elif line == 'dns':
+                rules.append("udp and port 53")
+            elif line == 'http':
+                rules.append("tcp and port 80")
+            elif line == 'ftp':
+                rules.append("tcp and port 21")
+
+    return rules
 def packet_callback(packet):
-    print("Sniffing interface:", packet.sniffed_on)
-    if ICMP in packet and packet[ICMP].type == 8:
-        src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
-        print(f"Ping detected: {src_ip} -> {dst_ip}")
-    elif TCP in packet and packet[TCP].dport == 22:
-        src_ip = packet[IP].src
-        dst_ip = packet[IP].dst
+    src_ip = packet[IP].src
+    dst_ip = packet[IP].dst
+
+    if TCP in packet:
+        src_port = packet[TCP].sport
+        dst_port = packet[TCP].dport
+    elif UDP in packet:
+        src_port = packet[UDP].sport
+        dst_port = packet[UDP].dport
+
+    if "icmp" in rules and ICMP in packet and packet[ICMP].type == 8:
+        print(f"Ping detected from {src_ip} to {dst_ip}")
+    elif "(tcp and port 22)" in rules and TCP in packet and packet[TCP].dport == 22:
         print(f"SSH connection attempt detected from {src_ip} to {dst_ip}")
 
-# Sniff traffic on the network interface
-sniff(prn=packet_callback, filter="icmp or (tcp and port 22)", store=0)
+rules = read_detection_rules('detection_rules.txt')
+filter_str = " or ".join(rules)
+print(filter_str)
+
+sniff(prn=packet_callback, filter=filter_str, store=0)
